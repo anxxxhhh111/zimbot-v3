730c026b48e77cea9eb411d472d8d3ebd155df3a
anxxxhhh111 <107339549+anxxxhhh111@users.noreply.github.com>
2022-07-03 12:46:10 +0530

    Update config.js
    Signed-off-by: anxxxhhh111 <107339549+anxxxhhh111@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.owner = ['916262947223','']
+global.premium = ['916262947223','916262947223']
+global.botname = 'ZIM X ANXH'
+global.ownername = 'ANXH'
+global.group1 = 'https://chat.whatsapp.com/'
+global.group2 = 'https://chat.whatsapp.com/'
+global.packname = 'ANXHXZIM'
+global.ytchannel = 'https://youtu.be/'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
